b6977a55f6d31b601718541d69f9aa5e94968d4c
Soumyendu <cyberrrnaut@gmail.com>
2023-12-10 01:11:17 +0530

    check

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const buttons = document.querySelectorAll(".button");
 const body = document.querySelectorAll("body");
 
-  button.addEventlistener('click',function(e){
-    console.log(e);
-  })
+
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        
+        if (e.target.id === "grey") {
+         
+            document.body.style.backgroundColor = e.target.id;
+        }
+    });
+});
