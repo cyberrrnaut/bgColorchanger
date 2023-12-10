c04fc2cdb3c687f1b48e8f82dc4d1cead138edd1
Soumyendu <cyberrrnaut@gmail.com>
2023-12-10 18:01:08 +0530

    final commit

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ const body = document.querySelectorAll("body");
 
 
 
-buttons.forEach(function(button) {
-    button.addEventListener('click', function(e) {
+buttons.forEach(function(b) {
+    b.addEventListener('click', function(e) {
         
         if (e.target.id === "grey") {
          
             document.body.style.backgroundColor = e.target.id;
         }
+        if(e.target.id ==="white"){
+            document.body.style.backgroundColor = e.target.id;
+        }
+
+        if(e.target.id === "blue"){
+            document.body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === "yellow"){
+            document.body.style.backgroundColor = e.target.id;
+        }
+
+
     });
 });
